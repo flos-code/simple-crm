031d7df586c7e103361a2f82be502079499400ee
flos-code <148456982+flos-code@users.noreply.github.com>
2024-02-05 16:12:43 +0100

    user table overview

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {
   MatDialogRef,
   MatDialogActions,
@@ -13,6 +13,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { User } from '../../models/user.class';
 import { FormsModule } from '@angular/forms';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -28,6 +31,8 @@ import { FormsModule } from '@angular/forms';
     MatDatepickerModule,
     MatButtonModule,
     FormsModule,
+    MatProgressBarModule,
+    CommonModule,
   ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
@@ -35,10 +40,25 @@ import { FormsModule } from '@angular/forms';
 export class DialogAddUserComponent {
   user = new User();
   birthDate!: Date;
+  loading: boolean = false;
+  firestore: Firestore = inject(Firestore);
   constructor(public dialogRef: MatDialogRef<DialogAddUserComponent>) {}
 
-  saveUser() {
+  async saveUser() {
     this.user.birthDate = this.birthDate.getTime();
     console.log('user', this.user);
+    this.loading = true;
+
+    // Correctly reference Firestore collection and use addDoc for adding documents
+    try {
+      const docRef = await addDoc(
+        collection(this.firestore, 'users'),
+        this.user.toJSON()
+      );
+      console.log('Document written with ID: ', docRef.id);
+      this.loading = false;
+    } catch (e) {
+      console.error('Error adding document: ', e);
+    }
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,17 +6,51 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
+import { MatCardModule } from '@angular/material/card';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatTooltipModule],
+  imports: [
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatCardModule,
+    CommonModule,
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
   user = new User();
+  allUsers: any = [];
+  private firestore: Firestore = inject(Firestore);
+  private dbSubscription!: Subscription;
   constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    const usersCollection = collection(this.firestore, 'users');
+    this.dbSubscription = collectionData(usersCollection, {
+      idField: 'id',
+    }).subscribe(
+      (changes) => {
+        console.log('Received Changes from DB', changes);
+        this.allUsers = changes;
+      },
+      (error) => {
+        console.error('Error fetching changes:', error);
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    // Unsubscribe to prevent memory leaks
+    this.dbSubscription.unsubscribe();
+  }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
